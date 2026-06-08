fea1b7f23a9b364f5447b8285563c160e56f9b3f
DezNyuh <devrh3424@gmail.com>
2026-06-08 12:09:36 -0300

    add: aulas inicias explicando para que serve print e hashtag

---
+++ Aulas/Aula1.py
@@ -0,0 +1,5 @@
+'''Isso é uma docstring.'''
+#Escrever um comentário
+print(123) #Não exibe
+#Não exibe
+print(456)
---
+++ Aulas/Aula2.py
@@ -0,0 +1,6 @@
+# \r\n → CRLF
+# \n → LF 
+
+print(12, 34, 1011, sep='-', end='\n##') #sep= ' ' serve para trocar o separador.
+print(56, 78, sep="-" '\n', end='\n') #end= ' ' adicionar um final para a linha
+print(9, 10, sep="-", end='\n')
