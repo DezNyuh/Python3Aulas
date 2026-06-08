e1baa722bcd21f89539ae32ffdfeae821786b892
DezNyuh <devrh3424@gmail.com>
2026-06-08 17:37:39 -0300

    add: oq é variavel e oq é classse

---
+++ Aulas/Aula3.py
@@ -0,0 +1,17 @@
+# str → string → texto
+
+print(1234) # int → inteiro → numeral
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+#Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+#Escape
+print("Luiz \"Otávio\"")
+
+#r
+print(r"Luiz \"Otávio\"")
---
+++ Aulas/Aula4.py
@@ -0,0 +1,10 @@
+print(11) #int
+print(-11) #int
+print(0) #int
+
+print(1.1, 10.11) #float
+print(0.0, -1.5) #float
+
+print(type('Otávio'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
