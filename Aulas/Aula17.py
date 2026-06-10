4f3d27b4f9b8ae58f696f15ff3172181919dd235
DezNyuh <devrh3424@gmail.com>
2026-06-10 18:32:44 -0300

    add: como usar f-string, como usar o debugger, como usar if e else

---
+++ Aulas/Aula17.py
@@ -0,0 +1,21 @@
+condicao_1 = False
+condicao_2 = False
+condicao_3 = True
+condicao_4 = False
+
+if condicao_1:
+    print('Este é a condição 1')
+elif condicao_2:
+    print('Este é a condição 2')
+elif condicao_3:
+    print('Este é a condição 3')
+elif condicao_4:
+    print('Este é a condição 4')
+else: 
+    print('Nenhuma condição é verdadeira')
+
+if 10 == 10:
+    print('Outro if')
+
+
+print('Fora do if')
