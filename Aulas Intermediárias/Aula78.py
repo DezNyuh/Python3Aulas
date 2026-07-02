2db67d2c0bb9b06ad004c44925d63a36a7eb845d
DezNyuh <devrh3424@gmail.com>
2026-07-02 18:49:48 -0300

    add: explicação inicial sobre set

---
+++ Aulas Intermediárias/Aula78.py
@@ -0,0 +1,11 @@
+s1 = set() #vazio
+l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+s2 = {1, 2, 3, 3, 3, 3, 3, 1} # com dados
+s2 = set(l1)
+l2 = list(s2)
+print(l2)
+s1 = {1, 2, 3}
+print(3 not in s1)
+
+for numero in s1:
+    print(numero)
