9ed7b395a95ecd126bcc52ff4f6f0822a3cdc094
DezNyuh <devrh3424@gmail.com>
2026-07-04 08:52:53 -0300

    add: exemplo de guardar letra em um set

--- Aulas Intermediárias/Aula78.py
+++ Aulas Intermediárias/Aula78.py
@@ -9,3 +9,29 @@ print(3 not in s1)
 
 for numero in s1:
     print(numero)
+
+# Métodos úteis:
+# add, update, clear, discard
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+#s1.clear() # descarta todas 
+s1.discard('Olá mundo') # descarta um especifico
+s1.discard('Luiz')
+print(s1)
+
+
+#operadores úteis:
+# união '|' unon - une
+# interseccção '&'- itens presente em ambos
+# diferença '-' itens presentes apenas no set da esquerda
+# diferença simétrica '^' itens que não estão em ambos
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s1 - s2
+s3 = s2 ^ s1
+print(s3)
