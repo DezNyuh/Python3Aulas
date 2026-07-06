76109b2301ecff6f9d661b6561b15a0b6a4bbe88
DezNyuh <devrh3424@gmail.com>
2026-07-06 20:29:44 -0300

    add: try e except para tratar execeções

---
+++ Aulas Intermediárias/Aula93.py
@@ -0,0 +1,16 @@
+try:
+    a = 18
+    b = 2
+    print('Linha 1')
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome b não está definido')
+except (TypeError + IndexError):
+    print('TypeError')
+except Exception:
+    print('ERRO DESCONHECIDO')
+
+print('CONTINUAR')
