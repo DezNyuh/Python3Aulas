97c58c503193514ac6f491dac81381d09822cc5c
DezNyuh <devrh3424@gmail.com>
2026-07-09 12:12:13 -0300

    add: introdução aos packages (pacotes) em python

---
+++ Aulas Intermediárias/Aula99.py
@@ -0,0 +1,13 @@
+from sys import path
+
+import Aula99_package.modulo
+from Aula99_package.modulo import soma_do_modulo
+from Aula99_package import modulo
+from Aula99_package.modulo import *
+
+# print(*path, sep='\n')
+print(soma_do_modulo(1, 2))
+print(Aula99_package.modulo.soma_do_modulo(1, 2))
+print(modulo.soma_do_modulo(1, 2))
+print(variavel) 
+print(nova_variavel)
