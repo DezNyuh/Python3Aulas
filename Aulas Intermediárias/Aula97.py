d0d331997de574f1317eb1c74034d13eee42c421
DezNyuh <devrh3424@gmail.com>
2026-07-09 11:35:22 -0300

    add: como importar coisas do seu próprio módulo (ponto de vista __main__)

--- Aulas Intermediárias/Aula97.py
+++ Aulas Intermediárias/Aula97.py
@@ -1,9 +1,8 @@
-try:
-    import sys
-    sys.path.append('/home')
-except ModuleNotFoundError:
-    ...
 import Aula97_m
+from Aula97_m import variavel_modulo, soma
 
-print('Este módulo se chama', __name__)
-print(*sys.path, sep='\n')
+# print('Este módulo se chama', __name__)
+print(Aula97_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(Aula97_m.soma(10, 5))
