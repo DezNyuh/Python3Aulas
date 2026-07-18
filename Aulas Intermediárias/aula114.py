d9d1e4ad69f752c9f673722c1acf7b8c0f374d81
DezNyuh <devrh3424@gmail.com>
2026-07-18 13:41:15 -0300

    add: funções recursivas, recursividade e stack overflow

---
+++ Aulas Intermediárias/aula114.py
@@ -0,0 +1,13 @@
+def recursiva(inicio=0, fim=4):
+    print(inicio, fim)
+    # Caso base
+    if inicio >= fim:
+        return fim
+
+
+    # Caso recursivo
+    # Contar até chegar ao final
+    inicio += 1
+    return recursiva(inicio, fim)
+
+print(recursiva())
