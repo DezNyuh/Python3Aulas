8e4d85016b90cce018e9113645609b7ee4e274ba
DezNyuh <devrh3424@gmail.com>
2026-07-20 15:41:08 -0300

    add: class - classes são moldes para criar novos objetos

---
+++ POO (Classes)/aula120.py
@@ -0,0 +1,19 @@
+# string = 'Luiz'
+# print(string.upper())
+# print(isinstance(string, str))
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Luiz'
+p1.sobrenome = 'Otávio'
+
+p2 = Pessoa()
+p2.nome = 'Maria'
+p2.sobrenome = 'Joana'
+
+print(p1.nome)
+print(p1.sobrenome)
+
+print(p2.nome)
+print(p2.sobrenome)
