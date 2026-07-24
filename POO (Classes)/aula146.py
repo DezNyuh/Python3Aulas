c684f753012287cbd3dd1b2ea5deeb29e4905322
DezNyuh <devrh3424@gmail.com>
2026-07-24 12:45:32 -0300

    update:  Notas das exceptions em Python 3.11+ (add_notes, __notes__)

--- POO (Classes)/aula146.py
+++ POO (Classes)/aula146.py
@@ -15,6 +15,8 @@ class OutroError(Exception):
 
 def levantar():
     exception_ = MeuError('a', 'b', 'c')
+    exception_.add_note('Olha a nota 1')
+    exception_.add_note('você errou isso')
     raise exception_
 
 try:
@@ -24,4 +26,6 @@ except (MeuError, ZeroDivisionError) as error:
     print(error.args)
     print()
     exception_ = OutroError('Vou lançar de novo')
+    exception_.__notes__ = error.__notes__.copy()
+    exception_.add_note('Mais uma nota')
     raise exception_ from error
