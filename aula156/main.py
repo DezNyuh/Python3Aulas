226b061ebd020bab39f2cc8300883fd11eaaa53d
DezNyuh <devrh3424@gmail.com>
2026-07-25 10:52:29 -0300

    add: DocString em class

--- aula156/main.py
+++ aula156/main.py
@@ -1,9 +1,11 @@
 # import uma_linha
-import documentando_funcoes
+# import documentando_funcoes
+import documentando_classes
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
 
-help(documentando_funcoes)
+# help(documentando_funcoes)
+help(documentando_classes)
