52a2e1ddd0e31eb10e7f515d7daa87e429533391
DezNyuh <devrh3424@gmail.com>
2026-07-27 12:51:39 -0300

    add: Configurações do decorator dataclass

--- POO (Classes)/aula159.py
+++ POO (Classes)/aula159.py
@@ -8,18 +8,18 @@
 from dataclasses import dataclass
 
 
-@dataclass(init=False)
+@dataclass(order=True)
 class Pessoa:
     nome: str
     sobrenome: str
 
-    def __init__(self, nome, sobrenome):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.nome_completo = f'{self.nome} {self.sobrenome}'
+    # def __init__(self, nome, sobrenome):
+    #     self.nome = nome
+    #     self.sobrenome = sobrenome
+    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
 
-    def __post_init__(self):
-        print('POST INIT')
+    # def __post_init__(self):
+    #     print('POST INIT')
         
 
     # @property
@@ -33,6 +33,6 @@ class Pessoa:
     #     self.sobrenome = ''.join(self.sobrenome)
 
 if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 'Otávio')
-    print(p1)
-    print(p1.nome_completo)
+    lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
+    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
+    print(ordenadas)
