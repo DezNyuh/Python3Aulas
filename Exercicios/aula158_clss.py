6354ef445ead8f6dce91fdc4538e06b20a46a4e2
DezNyuh <devrh3424@gmail.com>
2026-07-27 12:34:01 -0300

    add: solução do exercicio aula 158

--- Exercicios/aula158_clss.py
+++ Exercicios/aula158_clss.py
@@ -61,7 +61,7 @@ class ContaPoupanca(Conta):
             print(f'Saque bem-sucedido.\nSaldo da conta: R$ {self.saldo_formatado}')
         return self._saldo
 
-class Pessoa(ABC):
+class Pessoa():
     def __init__(self, nome, idade):
         self._nome = nome
         self._idade = idade
@@ -74,17 +74,51 @@ class Pessoa(ABC):
     def idade(self):
         return self._idade
 
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @idade.setter
+    def idade(self, idade):
+        self._nome = idade
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f' ({self.nome!r}, {self.idade!r})'
+        return f'{class_name}{attrs}'
+        
 class Cliente(Pessoa):
     def __init__(self, nome, idade):
         super().__init__(nome, idade)
+        self._conta: Conta | None = None
 
-    def criar_conta(self, agencia, num_conta):
-        self._agencia = agencia
-        self._num_conta = num_conta
-        self._saldo = 0
-
-yasuo = ContaCorrente(10, 25, 0)
-print(yasuo)
+    @property
+    def conta(self):
+        return self._conta
 
 
+class Banco:
+    def __init__(self):
+        self._clientes = []
+        self._contas = []
+        self._agencias = []
 
+    def adicionar_cliente(self, cliente):
+        self._clientes.append(cliente)
+
+    def adicionar_conta(self, conta):
+        self._contas.append(conta)
+
+    def adicionar_agencia(self, agencia):
+        self._agencias.append(agencia)
+
+    def autenticar(self, cliente,):
+        pass
+
+
+
+if __name__ == '__main__':
+    c1 = Cliente('Luiz', 30)
+    c1.conta = ContaCorrente(111, 222, 0)
+    print(c1)
+    print(c1.conta)
