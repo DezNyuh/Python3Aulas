e4212d2a73e87a535b8e580efc2b5c352723a79c
DezNyuh <devrh3424@gmail.com>
2026-08-04 18:19:07 -0300

    feat: Selenium - find_element e find_elements By

--- aula193/main.py
+++ aula193/main.py
@@ -6,6 +6,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 # Chrome Options
@@ -55,6 +56,11 @@ if __name__ == '__main__':
         )
     )
     search_input.send_keys('Hello World!')
+    search_input.send_keys(Keys.ENTER)
+
+    results = browser.find_element(By.ID, 'Search')
+    links = results.find_elements(By.TAG_NAME, 'a')
+    links[0].click()
 
     # Dorme por 10 segundos
     sleep(TIME_TO_WAIT)
